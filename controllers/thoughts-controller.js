0e07abe86e44fb00b11a971a8befb2d00df3478d
Tawhid <tawhidkamal5@gmail.com>
2023-07-15 17:16:15 -0400

    edits to user and thought controller updates, created thought controllers, halfway through

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -19,4 +19,47 @@ module.exports = {
       return res.status(500).json(err);
     }
   },
+
+  async getSingleThought(req,res){
+    try{
+        const thought = await Thoughts.findOne({ _id:req.params.thoughtId })
+        .select('-__v')
+        .lean();
+
+        if(!thought){
+            return res.status(404).json({ message: 'No thought found with that ID' })
+        }
+        res.json(thought);
+    }catch (err){
+        console.log(err);
+        return res.status(500).json(err)
+    }
+  },
+
+  async createThought(req,res){
+    try{
+        const thought = await Thoughts.create(req.body);
+        res.json(thought)
+    }catch(err){
+        console.log(err);
+        return res.status(500).json(err)
+    }
+  },
+
+  async updateThought(req,res){
+    try{
+        const thought = await Thoughts.findOneAndUpdate(
+            { _id: req.params.thoughtId},
+            { $set: req.body},
+            { runValidators: true, new: true },
+        )
+        if(!thought){
+            return res.status(404).json({ message: 'No thought found with that ID' })
+        }
+        res.json(thought)
+    }catch(err){
+        console.log(err);
+        return res.status(500).json(err)
+    }
+  },
 }
